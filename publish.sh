5c8ecd6cb44e3ff0d882c5b7070252c1249898b1
Jesse Clark <jjclark1982@gmail.com>
2015-03-02 13:00:27 +1100

    gh-pages publish script

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+SOURCE_BRANCH=$(git rev-parse HEAD)
+SOURCE_PARENT=$(git rev-parse -q --verify $SOURCE_BRANCH)
+SOURCE_MESSAGE=$(git log -1 --pretty=%B)
+PUBLISH_BRANCH=gh-pages
+PUBLISH_DIR=public
+PUBLISH_PARENT=$(git rev-parse -q --verify "$PUBLISH_BRANCH")
+
+if [ "$SOURCE_BRANCH" = "$PUBLISH_BRANCH" ]; then
+    echo "Cannot publish from branch $SOURCE_BRANCH"
+    exit 1
+fi
+
+rm -rf "$PUBLISH_DIR"
+git clone . "$PUBLISH_DIR"
+(cd "$PUBLISH_DIR"; git checkout "$PUBLISH_BRANCH"; rm -r *)
+
+# build with a script that will terminate
+NODE_ENV=production webpack
+
+cd "$PUBLISH_DIR"
+git add --all .
+git update-ref refs/heads/"$PUBLISH_BRANCH" $(
+    git commit-tree \
+        ${PUBLISH_PARENT:+-p $PUBLISH_PARENT} \
+        -p $SOURCE_PARENT \
+        -m "$SOURCE_MESSAGE (built by $0)" \
+        $(git write-tree)
+)
+git push origin "$PUBLISH_BRANCH"
