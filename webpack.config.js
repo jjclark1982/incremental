c5be34c0238196eb60d267b1fbd5bee001bac7a3
Jesse Clark <jjclark1982@gmail.com>
2015-03-17 22:45:19 +1100

    remove gulp

--- webpack.config.js
+++ webpack.config.js
@@ -75,8 +75,6 @@ else {
 // webpack-dev-server configuration
 
 config.devServer = {
-    host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 8080,
     contentBase: config.output.path,
     publicPath: '/',
     hot: true,
